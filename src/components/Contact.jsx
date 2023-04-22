710084ee185442717bb5898bb46df79b74bf310f
Maxime Mettey <maxime.mettey@gmail.com>
2023-04-23 01:08:20 +0200

    contact functional

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { slideIn } from "../utils/motion"
 
+// template_skgiji4
+// service_z5qknsl
+// KInrUzV8ZCGb_TPLd
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +20,44 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => { }
+  const handleChange = (e) => {
+    const { name, value } = e.target
 
-  const handleSubmit = (e) => { }
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true);
+
+    emailjs.send(
+      "service_z5qknsl",
+      "template_skgiji4",
+      {
+        from_name: form.name,
+        to_name: 'Maxime Mettey',
+        from_email: form.email,
+        to_email: 'contact@maxime-mettey.com',
+        message: form.message
+      },
+      "KInrUzV8ZCGb_TPLd"
+    ).then(() => {
+      setLoading(false)
+      alert("Thank you. I will get back to you as soon as possible.")
+
+      setForm({
+        name: '',
+        email: '',
+        message: ''
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error)
+
+      alert("Something went wring. Please try again later.")
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
